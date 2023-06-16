5bcf42e410e6edc22bf3622d882d3a8f4d64d256
laibaaaaz <laibazahid0786@gmail.com>
2023-06-16 16:00:01 +0500

    Login added

---
+++ DailyDairy/Controllers/LoginController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+using System.Web.Mvc;
+
+
+namespace DailyDairy.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View("Login");
+        }
+
+       
+        [HttpPost]
+        public ActionResult LoginPost()
+        {
+            string uname = Request.Form["username"];
+
+            string pass = Request.Form["password"];
+          
+            if (uname == "admin" && pass=="admin")
+            {
+
+              
+                return View("Dashboard");
+            }
+            else
+            {
+               
+                return View("Error");
+            }
+        }
+    }
+}
